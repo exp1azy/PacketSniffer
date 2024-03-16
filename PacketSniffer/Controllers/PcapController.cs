5d8acd022584da9503d319f127abea6492c5572e
Anton <anton.panin75@gmail.com>
2024-03-17 02:25:26 +0300

    ---

--- PacketSniffer/Controllers/PcapController.cs
+++ PacketSniffer/Controllers/PcapController.cs
@@ -60,7 +60,7 @@ namespace PacketSniffer.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet("stop")]
-        public IActionResult StopPhysic()
+        public IActionResult Stop()
         {
             _capAgent.Stop();
 
